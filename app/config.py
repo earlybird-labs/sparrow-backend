b15041c06b0bbf4adc516588cae23ab328e13caf
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-04-20 16:54:28 -0400

    Remove print statements from config.py

--- app/config.py
+++ app/config.py
@@ -4,11 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
-print(SLACK_BOT_TOKEN)
 SLACK_SIGNING_SECRET = os.getenv("SLACK_SIGNING_SECRET")
-print(SLACK_SIGNING_SECRET)
 SLACK_USER_TOKEN = os.getenv("SLACK_USER_TOKEN")
-print(SLACK_USER_TOKEN)
+
 
 TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
