45b4d71277a777f94eaea325c21dab5bff592c7d
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-04-20 07:29:09 -0400

    Add message handling and file upload functionality

--- app/handlers.py
+++ app/handlers.py
@@ -1,4 +1,117 @@
+import logging
 import requests
+import json
+
+from .llm import llm_response, describe_vision_anthropic, transcribe_audio
+from .utils import safe_say, fetch_and_format_thread_messages, is_bot_thread
+from .config import SLACK_USER_TOKEN
+
+
+def handle_message(ack, client, event, message, say):
+    ack()
+
+    bot_id = client.auth_test()["user_id"]
+
+    if message.get("text") == "speak":
+        handle_speak(client, message)
+
+    print(message)
+
+    if bot_id in message.get("text"):
+        if message.get("files"):
+            file_data = handle_file_upload(client, message)
+
+            print(file_data)
+
+            file_data_json = json.dumps(file_data)
+
+            user_message = message.get("text").replace(f"<@{bot_id}>", "")
+
+            user_message += "Attachments: " + file_data_json
+
+            response = llm_response([{"role": "user", "content": user_message}])
+
+            safe_say(say, text=response.ai_response, thread_ts=event["ts"])
+
+    if message.get("thread_ts"):
+
+        formatted_messages = fetch_and_format_thread_messages(client, message)
+
+        if is_bot_thread(client, formatted_messages):
+            response = llm_response(formatted_messages)
+            safe_say(say, text=response.ai_response, thread_ts=message["thread_ts"])
+
+
+def handle_file_upload(client, message):
+    files = message.get("files")
+
+    file_data = []
+
+    for file in files:
+        file_id = file.get("id")
+        file_url, file_type, mimetype = handle_file_sharing(client, file_id)
+
+        if file_type in ["jpg", "jpeg", "png", "webp", "gif"]:
+            image_description = describe_vision_anthropic(
+                file_url=file_url,
+                image_media_type=mimetype,
+                message=message.get("text"),
+            )
+            file_data.append({"upload_type": "image", "content": image_description})
+        elif file_type in ["webm", "mp4", "mp3", "wav", "m4a"]:
+            transcription = transcribe_audio(file_url)
+            file_data.append({"upload_type": "audio", "content": transcription})
+
+        else:
+            logging.error(f"Unsupported file type: {file_type}")
+
+        handle_file_revoke(client, file_id)
+
+    return file_data
+
+
+def handle_file_revoke(client, file_id):
+    client.files_revokePublicURL(token=SLACK_USER_TOKEN, file=file_id)
+
+
+def handle_file_sharing(client, file_id):
+    file_info = client.files_info(file=file_id).data.get("file")
+    file_type = file_info.get("filetype")
+    mimetype = file_info.get("mimetype")
+
+    client.files_sharedPublicURL(token=SLACK_USER_TOKEN, file=file_id)
+
+    public_link = file_info.get("permalink_public")
+    url_private = file_info.get("url_private")
+    pub_secret = public_link.split("-")[-1]
+    file_url = f"{url_private}?pub_secret={pub_secret}"
+
+    return file_url, file_type, mimetype
+
+
+def handle_speak(client, message):
+    try:
+        # Assuming speech_file_path is accessible here and contains the path to the MP3 file
+        file_path = "app/tmp/speech.mp3"
+        channels = message["channel"]
+        result = client.files_upload(
+            channels=channels, file=file_path, title="Speech File", filetype="mp3"
+        )
+        file_id = result["file"]["id"]
+        # Optionally, send a message about the uploaded file
+        # client.say(text=f"Uploaded an MP3 file: <@{file_id}>")
+    except Exception as e:
+        print(f"Error uploading file: {str(e)}")
+
+
+def handle_app_mention(ack, event, say):
+    ack()
+    pass
+    # try:
+    #     response = llm_response([{"role": "user", "content": event["text"]}])
+    #     safe_say(say, text=response.ai_response, thread_ts=event["ts"])
+    # except Exception as e:
+    #     logging.error(f"Error responding to app mention: {e}")
 
 
 def handle_open_modal(ack, body, client):
--- app/llm.py
+++ app/llm.py
@@ -1,33 +1,125 @@
 from simple_lm import SimpleLM
 from instructor import Mode
 
-from .config import TOGETHER_API_KEY, OPENAI_API_KEY
+from .config import TOGETHER_API_KEY, OPENAI_API_KEY, ANTHROPIC_API_KEY
 from .models import AIResponse
 from .prompts import sparrow_system_prompt
+from .utils import get_file_data
+
+from openai import OpenAI
+from anthropic import Anthropic
+
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
+anthropic_client = Anthropic(api_key=ANTHROPIC_API_KEY)
 
 
 simple_lm = SimpleLM()
-
-openai = simple_lm.setup_client(client_name="openai", api_key=OPENAI_API_KEY)
-
-together = simple_lm.setup_client(
+simple_lm.setup_client(client_name="openai", api_key=OPENAI_API_KEY)
+simple_lm.setup_client(
     client_name="together",
     api_key=TOGETHER_API_KEY,
 )
 
-ollama = simple_lm.setup_client(client_name="ollama", api_key="null", mode=Mode.MD_JSON)
+simple_lm.set_model("openai", "gpt-4-turbo")
+simple_lm.set_model("together", "meta-llama/Llama-3-70b-chat-hf")
+simple_lm.set_model("ollama", "dolphin")
 
 
-def llm_response(messages):
+def transcribe_audio(file_url):
+    audio_file = get_file_data(file_url)
+    transcription = openai_client.audio.transcriptions.create(
+        model="whisper-1", file=audio_file
+    )
+    return transcription.text
+
+
+def create_speech(text):
+    speech_file_path = "speech.mp3"
+    response = openai_client.audio.speech.create(
+        model="tts-1", voice="alloy", input=text
+    )
+
+    with open(speech_file_path, "wb") as file:
+        file.write(response.content)
+
+
+def describe_vision_anthropic(file_url, image_media_type, message=None):
+    if message:
+        prompt = message
+    else:
+        prompt = "Describe this image in as much detail as possible. Extract as much information as possible from the image."
+
+    image_data = get_file_data(file_url)
+    message = anthropic_client.messages.create(
+        model="claude-3-haiku-20240307",
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": image_media_type,
+                            "data": image_data,
+                        },
+                    },
+                    {
+                        "type": "text",
+                        "text": prompt,
+                    },
+                ],
+            }
+        ],
+    )
+    return message.content[0].text
+
+
+def describe_image(file_url):
+
+    response = openai_client.chat.completions.create(
+        model="gpt-4-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Describe this image."},
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": file_url},
+                    },
+                ],
+            }
+        ],
+        max_tokens=1000,
+    )
+
+    return response.choices[0].message.content
+
+
+def llm_response(messages, client_name="openai"):
+
+    client = simple_lm.get_client(client_name)
+    model = simple_lm.get_model(client_name)
 
     messages = [
         {"role": "system", "content": sparrow_system_prompt},
         *messages,
     ]
 
-    response = openai.create(
-        model="gpt-4-turbo-preview",
+    response = client.create(
+        model=model,
         messages=messages,
         response_model=AIResponse,
     )
     return response
+
+
+if __name__ == "__main__":
+    print(
+        describe_vision_anthropic(
+            file_url="https://upload.wikimedia.org/wikipedia/commons/a/a7/Camponotus_flavomarginatus_ant.jpg",
+            image_media_type="image/jpeg",
+        )
+    )
