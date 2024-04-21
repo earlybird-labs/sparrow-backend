2f24ae2803377cd2102b3be71afc201168289315
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-04-21 02:59:38 -0400

    Remove bug form and index files, add project intake form

--- app/blocks/index.py
+++
@@ -1,4 +0,0 @@
-import json
-
-with open("app/blocks/bug_form.json", "r") as file:
-    bug_form = json.load(file)["blocks"]
---
+++ app/workflows/forms/project.py
@@ -0,0 +1,28 @@
+"""
+* Conversational project intake form:
+    * Asks user for general project requirements.
+        * What ideas do you have to improve your business?
+            * Customer interests
+        * What product do you want to build?
+            * Project type
+        * What do you need our help building?
+            * Technical weakness
+        * Why do you want to build this?
+            * General problem
+        * What is your marketing strategy?
+            * Target market
+
+
+* Conversational feasibility analysis:
+    * Asks customer technical questions about their project.
+        * What progress do you currently have?
+        * A
+        * B
+        * C
+* CREATE RISK PROFILE
+* CREATE SCOPE OF WORK
+* CREATE PROJECT PROFILE
+    * ATTACH PROJECT TO COMPANY
+    * ATTACH RISK PROFILE
+    * ATTACH SCOPE OF WORK
+"""
