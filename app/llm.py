9e771b46652ba59bdf554002ffd828b796e34360
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-04-23 06:30:05 -0400

    Refactor LLM module and add new features

--- app/llm.py
+++ app/llm.py
@@ -1,35 +1,106 @@
-from simple_lm import SimpleLM
-from instructor import Mode
+import logging
 
-from .config import TOGETHER_API_KEY, OPENAI_API_KEY, ANTHROPIC_API_KEY, LOCAL_LLM
-from .models import AIResponse
-from .prompts import sparrow_system_prompt
-from .utils import get_file_data, save_audio_file, delete_file
+from pydantic import BaseModel, Field
+import instructor
 
 from openai import OpenAI
 from anthropic import Anthropic
+from groq import Groq
 
-import requests
-import json
 
+from .config import (
+    TOGETHER_API_KEY,
+    OPENAI_API_KEY,
+    ANTHROPIC_API_KEY,
+    GROQ_API_KEY,
+)
+from .models import AIResponse, RequestType
+from .prompts import general, project_manager, classify_request
+from .utils import get_file_data, save_audio_file, delete_file
+
+groq_client = Groq(api_key=GROQ_API_KEY)
 openai_client = OpenAI(api_key=OPENAI_API_KEY)
 anthropic_client = Anthropic(api_key=ANTHROPIC_API_KEY)
-
-
-simple_lm = SimpleLM()
-simple_lm.setup_client(client_name="openai", api_key=OPENAI_API_KEY)
-simple_lm.setup_client(
-    client_name="together",
-    api_key=TOGETHER_API_KEY,
-)
-simple_lm.setup_client(
-    client_name="ollama",
-    api_key="test",
+together_client = OpenAI(
+    api_key=TOGETHER_API_KEY, base_url="https://api.together.xyz/v1"
 )
 
-simple_lm.set_model("openai", "gpt-4-turbo")
-simple_lm.set_model("together", "meta-llama/Llama-3-70b-chat-hf")
-simple_lm.set_model("ollama", "dolphin")
+groq_instructor = instructor.from_groq(groq_client, mode=instructor.Mode.JSON)
+openai_instructor = instructor.from_openai(openai_client)
+anthropic_instructor = instructor.from_anthropic(anthropic_client)
+
+client_model_map = {
+    "openai": {
+        "client": openai_instructor,
+        "model": "gpt-4-turbo",
+    },
+    "anthropic": {
+        "client": anthropic_instructor,
+        "model": "claude-3-haiku-20240307",
+    },
+    "groq": {
+        "client": groq_instructor,
+        "model": "llama3-70b-8192",
+    },
+    "together": {
+        "client": openai_instructor,
+        "model": "meta-llama/Llama-3-70b-chat-hf",
+    },
+}
+
+system_prompt_map = {
+    "feature_request": project_manager,
+    "bug_report": project_manager,
+    "conversation": general,
+    "general_request": general,
+}
+
+
+def classify_user_request(message, client_name="groq"):
+    client = client_model_map[client_name]["client"]
+    model = client_model_map[client_name]["model"]
+    messages = [
+        {"role": "system", "content": classify_request},
+        {"role": "user", "content": message},
+    ]
+    response = client.create(
+        model=model,
+        messages=messages,
+        response_model=RequestType,
+    )
+    return response
+
+
+def llm_response(
+    messages, client_name="groq", request_type=RequestType.conversation, retry_count=1
+):
+    request_type_value = request_type.value
+    attempt = 0
+
+    while attempt <= retry_count:
+        try:
+            client = client_model_map[client_name]["client"]
+            model = client_model_map[client_name]["model"]
+            system_prompt = system_prompt_map[request_type_value]
+
+            full_messages = [
+                {"role": "system", "content": system_prompt},
+                *messages,
+            ]
+
+            return client.create(
+                model=model,
+                messages=full_messages,
+                response_model=AIResponse,
+            )
+
+        except Exception as e:
+            logging.error(f"Attempt {attempt} failed for {client_name} with error: {e}")
+            if attempt < retry_count:
+                client_name = "openai"  # Switch to openai for retry
+            attempt += 1
+
+    return None  # Return None if all retries fail
 
 
 def transcribe_audio(file_url, file_type):
@@ -62,7 +133,7 @@ def describe_vision_anthropic(file_url, image_media_type, message=None):
     image_data = get_file_data(file_url)
     message = anthropic_client.messages.create(
         model="claude-3-haiku-20240307",
-        max_tokens=1024,
+        max_tokens=4000,
         messages=[
             {
                 "role": "user",
@@ -102,54 +173,10 @@ def describe_image(file_url):
                 ],
             }
         ],
-        max_tokens=1000,
+        max_tokens=2000,
     )
 
     return response.choices[0].message.content
-
-
-def ollama_response(message):
-
-    print(message)
-
-    data = {"model": "dolphin", "prompt": message["content"], "stream": False}
-    response = requests.post("http://localhost:11434/api/generate", json=data)
-    response_data = response.json()
-
-    print(response_data)
-
-    ai_response = AIResponse(
-        ai_response=response_data["response"],
-        bug=False,
-    )
-
-    return ai_response
-
-
-def llm_response(messages, client_name="ollama"):
-
-    client = simple_lm.get_client(client_name)
-    model = simple_lm.get_model(client_name)
-
-    if LOCAL_LLM:
-        message = {"role": "user", "content": messages[-1]["content"]}
-
-        response = ollama_response(message)
-    else:
-        messages = [
-            {"role": "system", "content": sparrow_system_prompt},
-            *messages,
-        ]
-
-        response = client.create(
-            model=model,
-            messages=messages,
-            response_model=AIResponse,
-        )
-
-    print(response)
-
-    return response
 
 
 if __name__ == "__main__":
