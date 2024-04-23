1b9ff072302d255f7d5cf1849161e9bff1c1bbe2
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-04-23 06:30:56 -0400

    Add event, command, action, and view handlers

--- app/main.py
+++ app/main.py
@@ -20,19 +20,21 @@ app = App(
     signing_secret=SLACK_SIGNING_SECRET,
 )
 
-
+# Event handlers
 app.event("url_verification")(handle_url_verification)
 app.event("message")(handle_message)
 app.event("reaction_added")(handle_reaction_added)
 
+# Command handlers
 app.command("/onboard")(handle_onboard)
 app.command("/sparrow")(handle_sparrow)
 
+# Action handlers
 app.action("start_onboarding")(handle_onboarding_modal_open)
 app.action("create_jira_yes")(handle_create_jira_yes)
 app.action("create_jira_no")(handle_create_jira_no)
 
-
+# View handlers
 app.view("onboarding_modal")(handle_onboarding_modal_submit)
 
 if __name__ == "__main__":
