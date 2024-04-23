a308628984b17914480f8a54ee79a0858b6ebcfa
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-04-23 08:10:41 -0400

    Remove default values for ai_response and request_type fields in AIResponse model

--- app/models.py
+++ app/models.py
@@ -12,10 +12,5 @@ class RequestType(enum.Enum):
 
 class AIResponse(BaseModel):
     ai_response: str = Field(
-        default="",
         description="The assistant's response to the user's message.",
-    )
-    request_type: Optional[RequestType] = Field(
-        default=None,
-        description="The type of request the user is making.",
     )
