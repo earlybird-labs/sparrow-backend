5124fa06da6bee85099d369becb52415fc95acbc
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-04-23 23:01:04 -0400

    Add Slack API functionality and refactor utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,15 +1,14 @@
-import logging
-import time
-import base64
-import httpx
-from functools import wraps
-from slack_sdk.errors import SlackApiError
-import requests
-
+# utils.py
 
 import os
 import time
 import re
+import requests
+import httpx
+import base64
+from functools import wraps
+
+from typing import Optional
 
 from .logger import logger
 
@@ -49,46 +48,64 @@ def retry(exception_to_check, tries=4, delay=3, backoff=2, logger=None):
     return deco_retry
 
 
-@retry(SlackApiError, tries=5, delay=2, backoff=3)
-def safe_say(say, *args, **kwargs):
+def save_file(file_url: str, file_type: str) -> str:
     """
-    Ensures that the 'say' function is executed safely with retries on SlackApiError.
+    Fetches and saves an audio file from a URL.
 
-    :param say: The original 'say' function to be called.
-    :param args: Positional arguments for the 'say' function.
-    :param kwargs: Keyword arguments for the 'say' function.
-    :return: None
+    :param file_url: URL of the file to fetch.
+    :param file_type: Type of the file (e.g., 'mp3', 'wav', 'pdf')
+    :return: Path to the saved file.
     """
-    if "text" not in kwargs or not kwargs["text"]:
-        kwargs["text"] = "Sparrow is facing an issue, can you resend the message?"
+    response = httpx.get(file_url)
+    filename = f"{int(time.time())}.{file_type}"
+    file_path = os.path.join("tmp", filename)
+
+    with open(file_path, "wb") as file:
+        file.write(response.content)
+
+    return file_path
+
+
+def download_and_save_file(file_url: str, file_type: str) -> Optional[str]:
     try:
-        say(*args, **kwargs)
-    except SlackApiError as e:
-        logging.error(f"Slack API Error: {e}")
+        response = requests.get(file_url, stream=True)
+        timestamp = int(time.time())
+        if response.status_code == 200:
+            file_path = f"/tmp/{timestamp}.{file_type}"
+            with open(file_path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+            return file_path
+        else:
+            logger.error(
+                f"Failed to download file: {file_url} with status code: {response.status_code}"
+            )
+    except Exception as e:
+        logger.error(
+            f"Exception occurred while downloading file: {file_url}, Error: {str(e)}"
+        )
+    return None
 
 
-def format_markdown_to_slack_markdown(text):
+def delete_file(file_path: str) -> None:
     """
-    Converts markdown syntax in a string to Slack's markdown syntax using regex.
+    Deletes a file from the filesystem.
 
-    :param text: The string containing markdown syntax for bold and italic.
-    :return: A string with markdown syntax converted to Slack's markdown format.
+    :param file_path: Path to the file to be deleted.
     """
-    text = re.sub(r"\*(.*?)\*", r"_\1_", text)
-    formatted_text = re.sub(r"\*\*(.*?)\*\*", r"*\1*", text)
-    return formatted_text
+    os.remove(file_path)
 
 
-def is_bot_thread(client, messages):
+def get_file_data(file_url: str) -> str:
     """
-    Determines if a bot is part of the thread based on the messages.
+    Fetches file data from a URL and encodes it in base64.
 
-    :param client: The Slack client instance used to interact with the Slack API.
-    :param messages: List of message dictionaries from a thread.
-    :return: Boolean indicating if the bot is part of the thread.
+    :param file_url: URL of the file to fetch.
+    :return: Base64 encoded string of the file data.
     """
-    bot_id = client.auth_test()["user_id"]
-    return any(f"<@{bot_id}>" in msg["content"] for msg in messages)
+    response = httpx.get(file_url)
+    file_data = base64.b64encode(response.content).decode("utf-8")
+    return file_data
 
 
 def fetch_and_format_thread_messages(client, message):
@@ -117,65 +134,5 @@ def fetch_and_format_thread_messages(client, message):
 
         return formatted_messages
     except Exception as e:
-        logging.error(f"Error fetching or formatting thread messages: {e}")
+        logger.error(f"Error fetching or formatting thread messages: {e}")
         return []
-
-
-def save_file(file_url, file_type):
-    """
-    Fetches and saves an audio file from a URL.
-
-    :param file_url: URL of the file to fetch.
-    :param file_type: Type of the file (e.g., 'mp3', 'wav', 'pdf')
-    :return: Path to the saved file.
-    """
-    response = httpx.get(file_url)
-    filename = f"{int(time.time())}.{file_type}"
-    file_path = os.path.join("tmp", filename)
-
-    with open(file_path, "wb") as file:
-        file.write(response.content)
-
-    return file_path
-
-
-def download_and_save_file(file_url, file_type):
-    try:
-        response = requests.get(file_url, stream=True)
-        timestamp = int(time.time())
-        if response.status_code == 200:
-            file_path = f"/tmp/{timestamp}.{file_type}"
-            with open(file_path, "wb") as f:
-                for chunk in response.iter_content(chunk_size=8192):
-                    f.write(chunk)
-            return file_path
-        else:
-            logger.error(
-                f"Failed to download file: {file_url} with status code: {response.status_code}"
-            )
-    except Exception as e:
-        logger.error(
-            f"Exception occurred while downloading file: {file_url}, Error: {str(e)}"
-        )
-    return None
-
-
-def delete_file(file_path):
-    """
-    Deletes a file from the filesystem.
-
-    :param file_path: Path to the file to be deleted.
-    """
-    os.remove(file_path)
-
-
-def get_file_data(file_url):
-    """
-    Fetches file data from a URL and encodes it in base64.
-
-    :param file_url: URL of the file to fetch.
-    :return: Base64 encoded string of the file data.
-    """
-    response = httpx.get(file_url)
-    file_data = base64.b64encode(response.content).decode("utf-8")
-    return file_data
