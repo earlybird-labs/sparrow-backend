011427ebf5f2bdc03f29d1fedbb2de33bc6c9c73
Joe Petrantoni <79169021+jpetrantoni@users.noreply.github.com>
2024-04-23 21:15:00 -0400

    Less smelly

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,6 @@
-import time
 import json
 import os
+import time
 from pprint import pprint
 
 from .config import SLACK_USER_TOKEN
@@ -34,14 +34,10 @@ ephemeral_context = {}
 
 
 def get_thread_in_db(message, thread_ts, ts):
-    if not thread_ts:
-        thread_ts = ts
-
+    thread_ts = thread_ts or ts
     thread_id = find_db_thread(message.get("channel"), thread_ts)
-    print("OG thread_id", thread_id)
-    print("type(thread_id) at OG", type(thread_id))
-    print("OG ts", ts)
-    print("OG thread_ts", thread_ts)
+    logger.debug(f"OG thread_id: {thread_id}, type: {type(thread_id)}")
+    logger.debug(f"OG ts: {ts}, OG thread_ts: {thread_ts}")
 
     if not thread_id:
         oai_thread = create_thread()
@@ -50,29 +46,19 @@ def get_thread_in_db(message, thread_ts, ts):
             message.get("channel"), thread_ts, oai_thread.id, vectorstore_id
         )
         thread_id = insert_result.inserted_id
-        pprint("if not thread_id")
+        logger.debug("Thread not found, created new thread")
     else:
         thread_id.get("_id")
-        pprint("else thread_id")
+        logger.debug("Found existing thread")
 
-    print("end of get_thread_in_db", type(thread_id))
-    if type(thread_id) == dict:
-        thread_id = thread_id["_id"]
-    return thread_id
+    logger.debug(
+        f"End of get_thread_in_db, thread_id: {thread_id}, type: {type(thread_id)}"
+    )
+    return thread_id["_id"] if isinstance(thread_id, dict) else thread_id
 
 
 def handle_message(ack, client, event, message, say):
-    """
-    Handles incoming messages by determining the type of request and responding appropriately.
-
-    :param ack: Function to acknowledge the event.
-    :param client: Slack client for API interactions.
-    :param event: Event data containing message details.
-    :param message: Message details from the event.
-    :param say: Function to send messages to the channel.
-    """
     ack()
-
     bot_id = client.auth_test()["user_id"]
     ignore_list = ["message_deleted", "message_changed"]
 
@@ -81,8 +67,8 @@ def handle_message(ack, client, event, message, say):
         bot_mention = bot_id in message.get("text")
         has_files = message.get("files") is not None
         request_type = RequestType.conversation
-        thread_ts = message.get("thread_ts", None)
-        ts = message.get("ts", None)
+        thread_ts = message.get("thread_ts")
+        ts = message.get("ts")
 
         if not has_files:
             request_type = classify_user_request(event["text"])
@@ -99,7 +85,7 @@ def handle_message(ack, client, event, message, say):
             )
 
             if request_detected:
-                print("\n\nPM request\n\n")
+                logger.info("PM request detected")
                 thread_id = get_thread_in_db(message, thread_ts, ts)
                 handle_pm_request(client, say, event, message, bot_id)
             elif request_type == RequestType.ai_conversation:
@@ -111,12 +97,8 @@ def handle_message(ack, client, event, message, say):
             thread_id = get_thread_in_db(message, thread_ts, ts)
             handle_direct_message(client, say, event, message, bot_id, thread_id)
         elif thread_ts is not None:
-            if bot_already_in_thread(
-                client, message.get("thread_ts"), message.get("channel")
-            ):
+            if bot_already_in_thread(client, thread_ts, message.get("channel")):
                 thread_id = get_thread_in_db(message, thread_ts, ts)
-
-                print(thread_id)
                 logger.info("Handling thread message")
                 handle_thread_message(
                     client, say, event, message, bot_id, request_type, thread_id
@@ -124,15 +106,6 @@ def handle_message(ack, client, event, message, say):
 
 
 def handle_pm_request(client, say, event, message, bot_id):
-    """
-    Handles private messages that may lead to creating a Jira issue.
-
-    :param client: Slack client for API interactions.
-    :param say: Function to send messages to the channel.
-    :param event: Event data containing message details.
-    :param message: Message details from the event.
-    :param bot_id: ID of the bot user.
-    """
     user_id = message["user"]
     channel_id = message["channel"]
     message_ts = message["ts"]
@@ -150,14 +123,6 @@ def handle_pm_request(client, say, event, message, bot_id):
 
 
 def handle_create_jira_yes(ack, body, client, respond):
-    """
-    Handles the affirmative response to creating a Jira issue.
-
-    :param ack: Function to acknowledge the event.
-    :param body: Body of the response containing user and message details.
-    :param client: Slack client for API interactions.
-    :param respond: Function to send follow-up messages.
-    """
     ack()
 
     ephemeral_id = body["container"]["message_ts"]
@@ -178,15 +143,6 @@ def handle_create_jira_yes(ack, body, client, respond):
 
 
 def handle_create_jira_no(ack, body, client, say, respond):
-    """
-    Handles the negative response to creating a Jira issue.
-
-    :param ack: Function to acknowledge the event.
-    :param body: Body of the response containing user and message details.
-    :param client: Slack client for API interactions.
-    :param say: Function to send messages to the channel.
-    :param respond: Function to send follow-up messages.
-    """
     ack()
 
     bot_id = client.auth_test()["user_id"]
@@ -199,46 +155,22 @@ def handle_create_jira_no(ack, body, client, say, respond):
 
 
 def handle_reaction_added(ack, client, event):
-    """
-    Handles reactions added to messages.
-
-    :param ack: Function to acknowledge the event.
-    :param client: Slack client for API interactions.
-    :param event: Event data containing reaction details.
-    """
     ack()
-    pprint(event)
+    logger.debug(f"Reaction added event: {event}")
     reaction_name = event.get("reaction")
-    pprint(reaction_name)
+    logger.debug(f"Reaction name: {reaction_name}")
     if reaction_name == "ebl":
-        pprint("EBL reaction added")
+        logger.info("EBL reaction added")
 
 
 def handle_sparrow(ack, client, respond, command):
-    """
-    Handles specific commands directed at the bot.
-
-    :param ack: Function to acknowledge the command.
-    :param client: Slack client for API interactions.
-    :param respond: Function to send responses to the command.
-    :param command: Command details.
-    """
     ack()
     request = command.get("text")
-    pprint(request)
+    logger.debug(f"Sparrow request: {request}")
     # response = agent.run(request)
 
 
 def handle_direct_message(client, say, event, message, bot_id, thread_id=None):
-    """
-    Handles direct messages to the bot, with or without file attachments.
-
-    :param client: Slack client for API interactions.
-    :param say: Function to send messages to the channel.
-    :param event: Event data containing message details.
-    :param message: Message details from the event.
-    :param bot_id: ID of the bot user.
-    """
     if message.get("files"):
         logger.info("Handling direct message with file")
         handle_message_with_file(client, say, event, message, bot_id, thread_id)
@@ -258,16 +190,6 @@ def handle_thread_message(
     request_type=RequestType.bug_report,
     thread_id=None,
 ):
-    """
-    Handles messages within a thread, determining if the bot should respond based on its previous involvement.
-
-    :param client: Slack client for API interactions.
-    :param say: Function to send messages to the channel.
-    :param event: Event data containing message details.
-    :param message: Message details from the event.
-    :param bot_id: ID of the bot user.
-    :param request_type: Type of request identified in the message.
-    """
     formatted_messages = fetch_and_format_thread_messages(client, message)
     if message.get("files"):
         logger.info("Handling thread message with file")
@@ -288,7 +210,7 @@ def handle_thread_message(
         oai_thread = find_db_thread_by_id(thread_id)["oai_thread"]
         add_message_to_thread(oai_thread, user_message, "user")
         run_id = create_run(oai_thread).id
-        print("create_run")
+        logger.info("Run created")
         completed = False
         while not completed:
             steps = run_steps(oai_thread, run_id)
@@ -300,7 +222,7 @@ def handle_thread_message(
                         completed = True
                         break
                 except Exception as e:
-                    print(f"Error processing step: {e}")
+                    logger.error(f"Error processing step: {e}")
                     completed = False
                 time.sleep(1)
         context_found = "Document Search Results:\n" + result
@@ -319,24 +241,9 @@ def handle_message_with_file(
     request_type=RequestType.conversation,
     thread_id=None,
 ):
-    """
-    Processes messages that include file attachments, handling different types of uploads.
-
-    :param client: Slack client for API interactions.
-    :param say: Function to send messages to the channel.
-    :param event: Event data containing message details.
-    :param message: Message details from the event.
-    :param bot_id: ID of the bot user.
-    :param additional_messages: Additional messages related to the main message.
-    :param request_type: Type of request identified in the message.
-    """
-
-    print("thread_id in handle_message_with_file", thread_id)
-    print("type(thread_id) in handle_message_with_file", type(thread_id))
-    # loaded_thread_id = json.loads(thread_id)
-    # print("loaded_thread_id", loaded_thread_id)
-    # print("type(loaded_thread_id)", type(loaded_thread_id))
-    print("\n\n")
+    logger.debug(
+        f"Thread ID in handle_message_with_file: {thread_id}, type: {type(thread_id)}"
+    )
 
     file_datas, speech_mode, vectorstore = process_file_upload(
         SLACK_USER_TOKEN, client, message, thread_id
@@ -353,15 +260,15 @@ def handle_message_with_file(
             vectorstore_id = db_thread.get("vectorstore_id")
         else:
             vectorstore_id = db_thread.get("vectorstore_id")
-        print("vectorstore_id", vectorstore_id)
+        logger.info(f"vectorstore_id: {vectorstore_id}")
         oai_thread = find_db_thread_by_id(thread_id)["oai_thread"]
         modify_thread(oai_thread, [vectorstore_id])
-        print("modify_thread")
+        logger.info("Thread modified")
 
         add_message_to_thread(oai_thread, user_message, "user")
-        print("add_message_to_thread")
+        logger.info("Message added to thread")
         run_id = create_run(oai_thread).id
-        print("create_run")
+        logger.info("Run created")
         completed = False
         while not completed:
             steps = run_steps(oai_thread, run_id)
@@ -373,7 +280,7 @@ def handle_message_with_file(
                         completed = True
                         break
                 except Exception as e:
-                    print(f"Error processing step: {e}")
+                    logger.error(f"Error processing step: {e}")
                     completed = False
                 time.sleep(1)
         user_message += "Document Search Results:\n" + result
@@ -399,37 +306,22 @@ def handle_message_with_file(
 
 
 def bot_already_in_thread(client, thread_ts, channel_id):
-    """
-    Checks if the bot has already been mentioned in the thread or has sent a message in the thread.
-
-    :param client: The Slack client to interact with the Slack API.
-    :param thread_ts: The timestamp of the thread's parent message.
-    :param channel_id: The ID of the channel where the thread exists.
-    :return: Boolean indicating if the bot is already in the thread.
-    """
     try:
         bot_id = client.auth_test()["user_id"]
         response = client.conversations_replies(channel=channel_id, ts=thread_ts)
         messages = response.get("messages", [])
         for message in messages:
-            if message.get("user") == bot_id:
-                return True
-            if f"<@{bot_id}>" in message.get("text", ""):
+            if message.get("user") == bot_id or f"<@{bot_id}>" in message.get(
+                "text", ""
+            ):
                 return True
         return False
     except Exception as e:
-        print(f"Error checking if bot is in thread: {e}")
+        logger.error(f"Error checking if bot is in thread: {e}")
         return False
 
 
 def handle_onboarding_modal_open(ack, body, client):
-    """
-    Opens an onboarding modal when triggered by a user action.
-
-    :param ack: Function to acknowledge the action.
-    :param body: Body of the request containing trigger details.
-    :param client: Slack client for API interactions.
-    """
     ack()
     client.views_open(
         trigger_id=body["trigger_id"],
@@ -438,27 +330,12 @@ def handle_onboarding_modal_open(ack, body, client):
 
 
 def handle_onboarding_modal_submit(ack, body, view):
-    """
-    Handles the submission of data from the onboarding modal.
-
-    :param ack: Function to acknowledge the submission.
-    :param body: Body of the submission containing user and form details.
-    :param view: View details from the modal submission.
-    """
     ack()
-    pprint(body)
-    pprint(view)
+    logger.debug(f"Onboarding modal submitted: {body}")
+    logger.debug(f"Onboarding modal view: {view}")
 
 
 def compile_messages(additional_messages, user_message):
-    """
-    Compiles messages into a structured format for processing.
-
-    :param additional_messages: Additional messages related to the main message.
-    :param user_message: The primary user message.
-    :return: List of structured messages.
-    """
-    # Ensure additional_messages is always treated as a list
     if not isinstance(additional_messages, list):
         additional_messages = []
 
@@ -468,15 +345,6 @@ def compile_messages(additional_messages, user_message):
 
 
 def process_response(response, speech_mode, client, say, event):
-    """
-    Processes the response from the language model and sends it to the user.
-
-    :param response: The response from the language model.
-    :param speech_mode: Boolean indicating if the response should be spoken.
-    :param client: Slack client for API interactions.
-    :param say: Function to send messages to the channel.
-    :param event: Event data containing message details.
-    """
     if isinstance(response, AIResponse):
         response = response.ai_response
     if speech_mode:
@@ -488,14 +356,6 @@ def process_response(response, speech_mode, client, say, event):
 
 
 def handle_speak(client, channel, ai_response, thread_ts=None):
-    """
-    Handles the speaking of responses, converting text to speech and sending as an audio file.
-
-    :param client: Slack client for API interactions.
-    :param channel: Channel to send the audio file.
-    :param ai_response: The response text to be spoken.
-    :param thread_ts: Optional thread timestamp to send the response in a thread.
-    """
     try:
         file_path = "tmp/speech.mp3"
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
@@ -508,17 +368,11 @@ def handle_speak(client, channel, ai_response, thread_ts=None):
         }
         if thread_ts:
             upload_kwargs["thread_ts"] = thread_ts
-        pprint(upload_kwargs)
+        logger.debug(f"File upload arguments: {upload_kwargs}")
         result = client.files_upload_v2(**upload_kwargs)
     except Exception as e:
         logger.error(f"Error uploading file: {str(e)}")
 
 
 def handle_url_verification(event_data):
-    """
-    Verifies the URL during the Slack Event API setup.
-
-    :param event_data: Event data containing the verification challenge.
-    :return: Challenge answer for verification.
-    """
     return event_data["challenge"]
